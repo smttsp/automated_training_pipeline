99ef18f6817c34c2d63d6dbc9bbe064e7d649147
smttsp <samettaspinarx@gmail.com>
2023-03-23 10:58:53 -0400

    AI-2 [UPD] data downloader is completed

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+from training_pipeline.dataloader import download_fashion_mnist
 
-print(1)
+
+if __name__ == "__main__":
+    download_fashion_mnist("_data/fashion_mnist/")
+
+
+    # train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    # val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
