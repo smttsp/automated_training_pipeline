e989c2f2c279f8539f8c129dfdf00c82ca941f0c
smttsp <samettaspinarx@gmail.com>
2023-03-23 18:58:58 -0400

    AI-9 [FIX] warning for hydra is fixes

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from training_pipeline.train import train_model
 import hydra
 
 
-@hydra.main(version_base="1.1", config_path="./configs", config_name="fashion_mnist")
+@hydra.main(version_base=None, config_path="./configs", config_name="fashion_mnist")
 def main(cfg):
-    train_model(epochs=4)
+
+    train_model(cfg)
 
 
 if __name__ == "__main__":
