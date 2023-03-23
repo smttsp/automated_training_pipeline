c598767b7bc21061df34a39b53b4f3ed32a6f848
smttsp <samettaspinarx@gmail.com>
2023-03-22 23:15:12 -0400

    [UPD] `download_data` implemented

--- training_pipeline/dataloader.py
+++ training_pipeline/dataloader.py
@@ -1,5 +1,37 @@
-def download_data():
-    pass
+import os
+from torchvision import datasets, transforms
+
+
+def download_data(root="data", download=True):
+    """Downloading the data
+
+    Args:
+        root ():
+        download ():
+
+    Returns:
+
+    """
+    
+    os.makedirs(root, exist_ok=True)
+
+    train_data = datasets.FashionMNIST(
+        root="data",
+        train=True,
+        download=download,
+        transform=transforms.ToTensor(),
+        target_transform=None,
+    )
+
+    test_data = datasets.FashionMNIST(
+        root="data",
+        train=False,
+        download=download,
+        transform=transforms.ToTensor(),
+        target_transform=None
+    )
+    return train_data, test_data
+
 
 
 def load_train_test_data():
