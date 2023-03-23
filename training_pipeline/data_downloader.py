73cc0eca6b60d91b6f35ac872086f22058e73a01
smttsp <samettaspinarx@gmail.com>
2023-03-23 18:59:23 -0400

    AI-9 [UPD] download mnist

--- training_pipeline/data_downloader.py
+++ training_pipeline/data_downloader.py
@@ -4,23 +4,67 @@ import torch
 from torch.utils import data
 
 
-def download_fashion_mnist(root="_data/fashion_mnist"):
+def download_mnist(data_dir):
     """Downloading the fashion mnist data
 
     Args:
-        root (str): root directory
+        data_dir (str): root directory
 
     Returns:
 
     """
     torch.manual_seed(41)
-    os.makedirs(root, exist_ok=True)
+
+    data_dir = data_dir or "./_data/mnist"
+
+    os.makedirs(data_dir, exist_ok=True)
 
     # if data already exists, don't download it. Pull from the disk
-    download = not os.path.exists(os.path.join(root, 'FashionMNIST', 'processed'))
+    download = not os.path.exists(os.path.join(data_dir, 'MNIST', 'raw'))
+
+    full_train_data = datasets.MNIST(
+        root=data_dir,
+        train=True,
+        download=download,
+        transform=transforms.ToTensor(),
+        target_transform=None,
+    )
+
+    test_data = datasets.MNIST(
+        root=data_dir,
+        train=False,
+        download=download,
+        transform=transforms.ToTensor(),
+        target_transform=None
+    )
+    val_len = len(test_data)
+    train_len = len(full_train_data)-val_len
+
+    train_data, val_data = data.random_split(full_train_data, [train_len, val_len])
+
+    return train_data, val_data, test_data
+
+
+def download_fashion_mnist(data_dir):
+    """Downloading the fashion mnist data
+
+    Args:
+        data_dir (str): root directory
+
+    Returns:
+
+    """
+
+    data_dir = data_dir or "./_data/fashion_mnist"
+
+    torch.manual_seed(41)
+    os.makedirs(data_dir, exist_ok=True)
+
+    # if data already exists, don't download it. Pull from the disk
+    download = not os.path.exists(os.path.join(data_dir, 'FashionMNIST', 'raw'))
 
     full_train_data = datasets.FashionMNIST(
-        root=root,
+        root=data_dir,
         train=True,
         download=download,
         transform=transforms.ToTensor(),
@@ -28,7 +72,7 @@ def download_fashion_mnist(root="_data/fashion_mnist"):
     )
 
     test_data = datasets.FashionMNIST(
-        root=root,
+        root=data_dir,
         train=False,
         download=download,
         transform=transforms.ToTensor(),
