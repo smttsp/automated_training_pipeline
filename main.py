16983ab323d9ce37edde839bed6611c72bea8327
smttsp <samettaspinarx@gmail.com>
2023-03-23 16:34:49 -0400

    AI-9 [UPD] hydra integration done

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from training_pipeline.train import train_model
 
+import hydra
 
-if __name__ == "__main__":
 
+@hydra.main(version_base="1.1", config_path="./configs", config_name="fashion_mnist")
+def main(cfg):
     train_model(epochs=4)
 
+
+if __name__ == "__main__":
+    main()
 
