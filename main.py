a299cd813a7eb9559ce2af9ff44f0df5d19304b6
smttsp <samettaspinarx@gmail.com>
2023-03-23 20:14:42 -0400

    AI-8 [UPD] save model function added

--- main.py
+++ main.py
@@ -15,13 +15,20 @@ def get_runtime_str():
     return runtime_str
 
 
+def save_model(cfg, model):
+    project_name = cfg.project_name
+    save_dir = cfg.training.save_dir
+
+    os.makedirs(save_dir, exist_ok=True)
+    save_path = os.path.join(save_dir, f"{project_name}_{get_runtime_str()}.pth")
+    torch.save(obj=model, f=save_path)
+
+
 @hydra.main(version_base=None, config_path="./configs", config_name="fashion_mnist")
 def main_fn(cfg):
     model = train_model(cfg)
+    save_model(cfg, model)
 
-    os.makedirs(cfg.training.save_dir, exist_ok=True)
-    save_path = os.path.join(cfg.training.save_dir, f"{get_runtime_str()}.pth")
-    torch.save(obj=model.state_dict(), f=save_path)
     return None
 
 
