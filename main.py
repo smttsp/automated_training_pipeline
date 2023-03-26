c4c9b427c9bb57f6ccc564696af7176245332427
smttsp <samettaspinarx@gmail.com>
2023-03-26 05:29:39 -0400

    AI-9 [UPD] read config_name from command line

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import os
 from datetime import datetime
-
+import argparse
 import hydra
 import torch
 
 from training_pipeline.train import train_model
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--config_name",
+        default="mnist",
+        required=False,  # it should be true by default and no default
+        type=str,
+        help="name of the config to be uses",
+    )
+    args = parser.parse_args()
+    print(args.config_name)
+    return args.config_name
 
 
 def get_runtime_str():
@@ -24,7 +38,10 @@ def save_model(cfg, model):
     torch.save(obj=model, f=save_path)
 
 
-@hydra.main(version_base=None, config_path="./configs", config_name="cifar100")
+config_name = parse_args()
+
+
+@hydra.main(version_base=None, config_path="./configs", config_name=config_name)
 def main_fn(cfg):
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model = train_model(cfg, device)
