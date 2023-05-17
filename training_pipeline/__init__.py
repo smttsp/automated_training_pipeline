f17c784df347b59dca14bfd302f3c8bb8d48e440
Samet <samettaspinarx@gmail.com>
2023-05-17 07:58:33 -0400

    AI-0 [UPD] removed redundant stuff

--- training_pipeline/__init__.py
+++ training_pipeline/__init__.py
@@ -1,5 +1,5 @@
 """The `example_template` Python Package
 
-This is an example Python package for use internally at Overjet by developers looking to build out
+This is an example Python package for use internally at smttsp by developers looking to build out
 new Python packages, to isolate and share code.
 """
